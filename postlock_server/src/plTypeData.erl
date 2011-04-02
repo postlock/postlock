50a24feffd6776e4a38a2a4a9825e7e9481b3ee3
Akos Toth-Mate <akos@tothmate.com>
2011-04-02 15:07:24 +0200

    eliminated trivial guards

--- postlock_server/src/plTypeData.erl
+++ postlock_server/src/plTypeData.erl
@@ -8,14 +8,21 @@
 
 -module(plTypeData).
 -export([new/0, apply/2, xform/2]).
+-compile({no_auto_import, [apply/2]}).
 
 new() ->
     undefined.
 
 apply({set, Value}, _) ->
-    Value.
+    Value;
+apply({unsafe_set, Value}, Data) ->
+    apply({set, Value}, Data).
 
-xform({_Op, Value1}, {set, _Value2}) when _Op == set; _Op == unsafe_set -> 
+xform({set, Value1}, {set, _Value2}) -> 
     {fail, {set, Value1}, nop};
-xform({_Op, _Value1}, {unsafe_set, Value2}) when _Op == set; _Op == unsafe_set ->
-    {ok, nop, {set, Value2}}.
+xform({unsafe_set, Value1}, {set, Value2}) -> 
+    xform({set, Value1}, {set, Value2});
+xform({set, _Value1}, {unsafe_set, Value2}) ->
+    {ok, nop, {set, Value2}};
+xform({unsafe_set, Value1}, {unsafe_set, Value2}) ->
+    xform({set, Value1}, {unsafe_set, Value2}).
--- postlock_server/src/plTypeDict.erl
+++ postlock_server/src/plTypeDict.erl
@@ -8,24 +8,44 @@
 
 -module(plTypeDict).
 -export([new/0, apply/2, xform/2]).
+-compile({no_auto_import, [apply/2]}).
 
 new() ->
     dict:new().
 
 apply({set, Key, Value}, Dict) ->
     dict:store(Key, Value, Dict);
+apply({unsafe_set, Key, Value}, Dict) ->
+    apply({set, Key, Value}, Dict);
 apply({remove, Key, Value}, Dict) ->
-    dict:erase(Key, Value, Dict).
+    dict:erase(Key, Value, Dict);
+apply({unsafe_remove, Key, Value}, Dict) ->
+    apply({remove, Key, Value}, Dict).
 
-xform({_Op, Key, Value1}, {set, Key, _Value2}) when _Op == set; _Op == unsafe_set ->
+
+xform({set, Key, Value1}, {set, Key, _Value2}) ->
     {fail, {set, Key, Value1}, nop};
-xform({_Op, Key, _Value1}, {unsafe_set, Key, Value2}) when _Op == set; _Op == unsafe_set ->
+xform({unsafe_set, Key, Value1}, {set, Key, Value2}) ->
+    xform({set, Key, Value1}, {set, Key, Value2});
+xform({set, Key, _Value1}, {unsafe_set, Key, Value2}) ->
     {ok, nop, {set, Key, Value2}};
-xform({_Op, Key, Value}, {remove, Key}) when _Op == set; _Op == unsafe_set ->
+xform({unsafe_set, Key, Value1}, {unsafe_set, Key, Value2}) ->
+    xform({set, Key, Value1}, {unsafe_set, Key, Value2});
+xform({set, Key, Value}, {remove, Key}) ->
     {fail, {set, Key, Value}, nop};
-xform({_Op, Key, _Value}, {unsafe_remove, Key}) when _Op == set; _Op == unsafe_set ->
+xform({unsafe_set, Key, Value}, {remove, Key}) ->
+    xform({set, Key, Value}, {remove, Key});
+xform({set, Key, _Value}, {unsafe_remove, Key}) ->
     {ok, nop, {remove, Key}};
-xform({_Op1, Key}, {_Op2, Key, Value}) when _Op1 == remove; _Op1 == unsafe_remove, _Op2 == set; _Op2 == unsafe_set ->
+xform({unsafe_set, Key, Value}, {unsafe_remove, Key}) ->
+    xform({set, Key, Value}, {unsafe_remove, Key});
+xform({remove, Key}, {set, Key, Value}) ->
     {ok, nop, {set, Key, Value}};
+xform({unsafe_remove, Key}, {set, Key, Value}) ->
+    xform({remove, Key}, {set, Key, Value});
+xform({remove, Key}, {unsafe_set, Key, Value}) ->
+    xform({remove, Key}, {set, Key, Value});
+xform({unsafe_remove, Key}, {unsafe_set, Key, Value}) ->
+    xform({remove, Key}, {set, Key, Value});
 xform({_, _Key1, _}, {_, _Key2, _}) -> % covers: (unsafe_)remove vs (unsafe_)remove, different keys
     {ok, nop, nop}.
